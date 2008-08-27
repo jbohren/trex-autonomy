94d4e120d4ae9b54e45a84d11dc639e51fbaae4d
mcgann.conor <mcgann.conor@93747af7-4052-0410-bfd5-d9cfb84e8a76>
2008-08-27 18:32:19 +0000

    Baseline from Willow Garage and MBARI

---
+++ agent/TREXDefs.hh
@@ -0,0 +1,47 @@
+#include "Id.hh"
+#include "LabelStr.hh"
+
+
+// As a sanity check we can switch to using the coed generator
+//#define USE_CODE_GENERATOR
+
+using namespace EUROPA;
+
+namespace TREX {
+
+  /*!< The type for a tick - a unit of time */
+  typedef unsigned int TICK;
+
+  class Agent;
+  typedef Id<Agent> AgentId;
+
+  class TeleoReactor;
+  typedef Id<TeleoReactor> TeleoReactorId;
+
+  class Server;
+  typedef Id<Server> ServerId;
+
+  class Observer;
+  typedef Id<Observer> ObserverId;
+
+  class Observation;
+
+  class DbCore;
+  typedef Id<DbCore> DbCoreId;
+
+  /**
+   * @brief Module initialization function
+   */
+  void initTREX();
+
+  /**
+   * @brief Model cleanup function
+   */
+  void uninitTREX();
+
+  /* USEFUL UTILITIES */
+
+  LabelStr compose(const LabelStr& prefix, const LabelStr& suffix);
+}
+
+#define CPU_STAT_LOG "cpuStat.log"
