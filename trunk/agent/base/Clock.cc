f17ad49a3d8e7c171a38548113dea48be5bc95e6
fredpy <fredpy@93747af7-4052-0410-bfd5-d9cfb84e8a76>
2008-10-21 19:55:19 +0000

    improved RealTimeClock accuracy

--- trunk/agent/base/Clock.cc
+++ trunk/agent/base/Clock.cc
@@ -1,6 +1,7 @@
 #include "Clock.hh"
 #include <errno.h>
 #include <time.h>
+#include <cmath>
 
 #include "LogManager.hh"
 
@@ -16,10 +17,10 @@ namespace TREX {
 
   void Clock::sleep(double sleepDuration){
     struct timespec tv;
-    tv.tv_sec = (time_t) sleepDuration;;
+    tv.tv_sec = (time_t) sleepDuration;
     tv.tv_nsec = (long) ((sleepDuration - tv.tv_sec) * 1e+9);
 
-    while(true){
+    while( tv.tv_sec>0 || tv.tv_nsec>0 ){
 	int rval = nanosleep(&tv, &tv);
 
 	if(rval == 0) // We are done sleeping
@@ -51,8 +52,18 @@ namespace TREX {
     sleep(m_sleepSeconds);
   }
 
+  TICK PseudoClock::selectStep(unsigned int stepsPerTick) {
+    if( stepsPerTick<=0 ) {
+      TREXLog()<<"requested number of steps is invalid ("<<stepsPerTick<<")."
+	" Setting it to 50."<<std::endl;
+      stepsPerTick = 50;
+    }
+    return stepsPerTick;
+  }
+
   PseudoClock::PseudoClock(double sleepSeconds, unsigned int stepsPerTick, bool stats) : 
-    Clock(sleepSeconds, stats), m_tick(0), m_internalTicks(0), m_stepsPerTick(stepsPerTick) {}
+    Clock(sleepSeconds, stats), m_tick(0), m_internalTicks(0), m_stepsPerTick(selectStep(stepsPerTick)) {
+  }
  
   TICK PseudoClock::getNextTick() {
     if(m_internalTicks > 0 && ((m_internalTicks % m_stepsPerTick) == 0))
@@ -64,17 +75,42 @@ namespace TREX {
   /**
    * Real Time Clock
    */
+  void RealTimeClock::getDate(timeval &date) {
+    gettimeofday(&date, NULL);
+  }
+
   RealTimeClock::RealTimeClock(double secondsPerTick, bool stats)
     : Clock(secondsPerTick/1000, stats),
-      m_tick(0),
-      m_secondsPerTick(secondsPerTick) {
+      m_started(false), m_tick(0) {
+    m_secondsPerTick.tv_sec = static_cast<long>(std::floor(secondsPerTick));
+    m_secondsPerTick.tv_usec = static_cast<long>(std::floor((secondsPerTick-m_secondsPerTick.tv_sec)*1e6));
     pthread_mutex_init(&m_lock, NULL);
   }
 
   void RealTimeClock::start(){
+    getDate(m_nextTickDate);
+    setNextTickDate();
+    m_started = true;
     pthread_create(&m_thread, NULL, threadRunner, this);
   }
 
+  void RealTimeClock::setNextTickDate() {
+    m_nextTickDate.tv_usec += m_secondsPerTick.tv_usec;
+    m_nextTickDate.tv_sec += m_secondsPerTick.tv_sec + (m_nextTickDate.tv_usec/1000000);
+    m_nextTickDate.tv_usec %= 1000000;
+  }
+
+  double RealTimeClock::timeToNextTick() const {
+    timeval tv;
+    double result;
+    getDate(tv);
+    
+    result = m_nextTickDate.tv_usec - tv.tv_usec;
+    result /= 1e6;
+    result += m_nextTickDate.tv_sec-tv.tv_sec;
+    return result;
+  }
+    
   TICK RealTimeClock::getNextTick(){
     pthread_mutex_lock(&m_lock);
     TICK tick = m_tick;
@@ -82,18 +118,33 @@ namespace TREX {
     return tick;
   }
 
+  void RealTimeClock::sleep() const {
+    if( m_started ) {
+      double delay;
+    
+      pthread_mutex_lock(&m_lock);
+      delay = timeToNextTick();
+      pthread_mutex_unlock(&m_lock);
+      
+      if( delay>0.0 )
+	Clock::sleep(delay);
+    } else 
+      Clock::sleep();
+  }
+
   void* RealTimeClock::threadRunner(void* clk){
     RealTimeClock* This = (RealTimeClock*) clk;
 
     // Loop forever, sleep for a tick
-    double sleepTime = This->m_secondsPerTick;
     while(true){
-      Clock::sleep(sleepTime);
+      Clock::sleep(This->timeToNextTick());
       pthread_mutex_lock(&This->m_lock);
       This->advanceTick(This->m_tick);
+      This->setNextTickDate();
       pthread_mutex_unlock(&This->m_lock);
     }
 
     return NULL;
   }
 }
+
