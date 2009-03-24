f7ee65516583b1b4eb3d26ed3be5a1435e908953
mcgann.conor <mcgann.conor@93747af7-4052-0410-bfd5-d9cfb84e8a76>
2009-03-24 21:19:40 +0000

    Log message

--- agent/base/DbCore.cc
+++ agent/base/DbCore.cc
@@ -1869,8 +1869,8 @@ namespace TREX {
     TREXLog() << nameString() << "Rejected " << token->toString() << std::endl;
     TREX_INFO("trex:warning", nameString() << token->toString() << " was rejected.\n\n" <<
 	      "   If this is a surprise, then you need to enable planner debug messages to investigate: \n" << 
-	      "   :Solver:step - useful for observing the backtracking search." << 
-	      "   :trex:planning - gives additional trex related information in the search in terms of flaw filtering etc.");
+	      "     :Solver:step - useful for observing the backtracking search.\n" << 
+	      "     :trex:planning - gives additional trex related information in the search in terms of flaw filtering etc.");
 
     m_tokenAgenda.erase(token);
   }
