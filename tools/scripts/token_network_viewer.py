aa5ea99995ef0c53a46717cf80a9cb161fa994cd
jonathan.bohren <jonathan.bohren@93747af7-4052-0410-bfd5-d9cfb84e8a76>
2009-08-22 23:13:22 +0000

    Adding trex visualization tools with regression test data

---
+++ tools/scripts/token_network_viewer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# System modules
+import sys,os
+import unittest
+import signal
+import threading, time
+import gtk
+
+# TREX modules
+from TREX.core import *
+from TREX.widgets_gtk import *
+
+def main():
+
+  # Process cli arguments
+  if len(sys.argv) > 1:
+    log_path = sys.argv[1]
+  else:
+    log_path = "."
+
+  # Initialize gtk multithread support
+  gtk.gdk.threads_init()
+
+  # Create db reader window
+  db_reader_window = DbReaderWindow(log_path=log_path)
+  db_reader_window.w.connect("destroy",gtk.main_quit)
+
+  # Create token network graph generator
+  token_network = TokenNetwork()
+  db_reader_window.register_listener(token_network.set_db_cores)
+
+  # Create token network window
+  token_network_window = TokenNetworkWindow(token_network)
+  token_network_window.register_listener(PropertyWindowFactory)
+
+  # Create token network filter window
+  token_network_filter = TokenNetworkFilter(token_network)
+  token_network_filter_window = TokenNetworkFilterWindow(token_network_filter)
+
+  db_reader_window.w.present()
+
+  # Set up signal handler
+  signal.signal(signal.SIGINT, signal.SIG_DFL) 
+
+  gtk.main()
+
+if __name__ == '__main__':
+  main()
