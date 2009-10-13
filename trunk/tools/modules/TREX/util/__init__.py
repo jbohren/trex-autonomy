995117c097e972b05ceda2adc3ffbf14e09b0695
mcgann.conor <mcgann.conor@93747af7-4052-0410-bfd5-d9cfb84e8a76>
2009-10-13 18:30:06 +0000

    Fixed for case where TREX is not defined

--- trunk/tools/modules/TREX/util/__init__.py
+++ trunk/tools/modules/TREX/util/__init__.py
@@ -4,11 +4,11 @@
 import os
 
 def trex_path(tail_path):
-  # Check if TREX path is defined
-  if os.environ.has_key('TREX'):
-    trex_home = os.environ['TREX']
-  else:
+  # Check if TREX path is defined. If not, set from reelative
+  if os.environ.has_key('TREX') == 0:
     os.environ['TREX'] = os.path.abspath(os.path.join(__file__,"../","../","../"))
+
+  trex_home = os.environ['TREX']
   
   # Compose path
   return os.path.join(trex_home,tail_path)
